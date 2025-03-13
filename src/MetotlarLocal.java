04593ada033c19fe8c4d70c7b664c17551cca73d
Nogoza <151651571+Nogoza@users.noreply.github.com>
2025-03-13 14:54:01 +0300

    Create MetotlarLocal.java

---
+++ src/MetotlarLocal.java
@@ -0,0 +1,13 @@
+package Metotlar;
+
+public class MetotlarLocal {
+    public static void main(String[] args) {
+        int a = 78;
+        int b = 0;
+
+        if (a < 40){
+            b = 2;
+        }
+        System.out.println(b);
+    }
+}
